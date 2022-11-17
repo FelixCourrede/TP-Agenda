93429e2f62ee0ce27c1b6b3c7afc742573367d0c
FelixCourrede <116156131+FelixCourrede@users.noreply.github.com>
2022-11-17 09:16:02 +0100

    Implémentation partielle de classe event

--- src/main/java/agenda/Event.java
+++ src/main/java/agenda/Event.java
@@ -8,23 +8,22 @@ public class Event {
      * The myTitle of this event
      */
     private String myTitle;
-    
+
     /**
      * The starting time of the event
      */
     private LocalDateTime myStart;
 
     /**
-     * The durarion of the event 
+     * The durarion of the event
      */
     private Duration myDuration;
-
 
     /**
      * Constructs an event
      *
-     * @param title the title of this event
-     * @param start the start time of this event
+     * @param title    the title of this event
+     * @param start    the start time of this event
      * @param duration the duration of this event
      */
     public Event(String title, LocalDateTime start, Duration duration) {
@@ -33,39 +32,24 @@ public class Event {
         this.myDuration = duration;
     }
 
-    /**
-     * Tests if an event occurs on a given day
-     *
-     * @param aDay the day to test
-     * @return true if the event occurs on that day, false otherwise
-     */
     public boolean isInDay(LocalDate aDay) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        LocalDate jourEvent = myStart.toLocalDate();
+        if (aDay.equals(jourEvent)) {
+            return (true);
+        }
+        return (false);
     }
-   
-    /**
-     * @return the myTitle
-     */
+
     public String getTitle() {
         return myTitle;
     }
 
-    /**
-     * @return the myStart
-     */
     public LocalDateTime getStart() {
         return myStart;
     }
 
-
-    /**
-     * @return the myDuration
-     */
     public Duration getDuration() {
         return myDuration;
     }
 
-   
-    
 }
